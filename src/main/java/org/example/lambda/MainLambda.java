3c2c1d194fd5c0eb21396d5a5da9edb965ebf929
Eric Thompson <106886171+Ethompson87@users.noreply.github.com>
2023-03-25 23:57:27 -0400

    Added if statement to check if name is an integer. Also added a while loop to restart the program after the error with the string being an interger.

--- src/main/java/org/example/lambda/MainLambda.java
+++ src/main/java/org/example/lambda/MainLambda.java
@@ -8,35 +8,54 @@ public class MainLambda {
     public static void main(String[] args) {
 
         Scanner information = new Scanner(System.in);
-        try {
-            System.out.print("Enter your name: ");
-            String name = information.next();
 
-            System.out.print("Enter your current age: ");
-            int age = information.nextInt();
+        String name;
+        int age;
+        boolean restartInput;
+
+        while (restartInput = true) {
+
+            try {
+
+                System.out.print("Enter your name: ");
+                name = information.next();
 
 
+                if (Integer.parseInt(name) == Integer.parseInt(name)) {
+                    System.out.println("This isn't a character.");
+                    restartInput = true;
+                }
 
-            //Lambda with a return statement
-            LambdaTest nameAge =  (na, ag) -> {
 
-            return "\n"+"This Lambda is with a return statement"+"\n"+"Your name is " + na + "\n"+ "Your age is : "+ ag +"\n";
-            };
+                System.out.print("Enter your current age: ");
+                age = information.nextInt();
 
-            System.out.println(nameAge.test(name, age));
 
-        //Lambda with void method
-            LambdaTest2 nameAge2 = (na, ag)-> {
+                //Lambda with a return statement
+                LambdaTest nameAge = (na, ag) -> {
 
-                System.out.println("This Lambda is with void method");
-                System.out.println("Your name is " + na + "\n"+ "Your age is : "+ ag);
-            };
-            nameAge2.test2(name,age);
+                    return "\n" + "This Lambda is with a return statement" + "\n" + "Your name is " + na + "\n" + "Your age is : " + ag + "\n";
+                };
 
-        }
+                System.out.println(nameAge.test(name, age));
 
-        catch(Exception error){
-            System.out.println("There was an error with your request");
+                //Lambda with void method
+                LambdaTest2 nameAge2 = (na, ag) -> {
+
+                    System.out.println("This Lambda is with void method");
+                    System.out.println("Your name is " + na + "\n" + "Your age is : " + ag);
+                };
+                nameAge2.test2(name, age);
+            } catch (Exception error) {
+                System.out.println("There was an error with your request");
+            }
         }
     }
+
 }
+
+
+
+
+
+
